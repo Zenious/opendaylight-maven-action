0b3612d567cd33ac7d79e0ae10484ca744fb1ebf
Zenious <7553066+Zenious@users.noreply.github.com>
2020-03-08 03:50:03 +0800

    update js to use spawn

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
 const core = require('@actions/core');
 
-function exec_command(command) {
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            core.error(`error: ${error.message}`);
-            core.setFailed(`Action failed with error`);
-            return;
-        }
-        if (stderr) {
-            core.error(`stderr: ${stderr}`);
-            core.setFailed(`Action failed with error`);
-            return;
-        }
-        core.debug(`stdout: ${stdout}`);
+function spawn_command(command, options, path) {
+
+    const spawn_shell = spawn(command, options);
+
+    spawn_shell.stdout.on("data", data => {
+        core.debug(`stdout: ${data}`);
     });
+
+    spawn_shell.stderr.on("data", data => {
+        core.error(`stderr: ${data}`);
+        core.setFailed(`Action failed with error`);
+    });
+
+    spawn_shell.on('error', (error) => {
+        core.error(`error: ${error.message}`);
+        core.setFailed(`Action failed with error`);
+    });
+
+    spawn_shell.on("close", code => {
+        core.debug(`child process exited with code ${code}`);
+    });
+
 }
 
 // Copy opendaylight maven setting
-exec_command("wget -q -O - https:\/\/raw.githubusercontent.com\/opendaylight\/odlparent\/master\/settings.xml > ~\/.m2\/settings.xml");
+spawn_command("wget", ["-q", "-O", "~\/.m2\/settings.xml","https:\/\/raw.githubusercontent.com\/opendaylight\/odlparent\/master\/settings.xml"]);
 
-let options = "";
+let options = ["clean", "install"];
 
 if (core.getInput("verbose").toLowerCase() == "false") {
-    options += "--quiet ";
+    options.push("--quiet ");
 }
 
 if (core.getInput("skipTests").toLowerCase() == "true") {
-    options += "-DskipTests ";
+    options.push("-DskipTests ");
 }
 
-const maven_command = "mvn clean install " + options;
-exec_command(maven_command);
+spawn_command("mvn", options);
