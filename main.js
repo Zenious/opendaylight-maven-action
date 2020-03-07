2d2198d69ad70d6cc321ce3ae4694e90437e41a6
Zenious <7553066+Zenious@users.noreply.github.com>
2020-03-08 03:24:20 +0800

    update js to use @actions/core

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
 const { exec } = require("child_process");
+const core = require('@actions/core');
 
 function exec_command(command) {
     exec(command, (error, stdout, stderr) => {
         if (error) {
-            console.log(`error: ${error.message}`);
+            core.error(`error: ${error.message}`);
+            core.setFailed(`Action failed with error`);
             return;
         }
         if (stderr) {
-            console.log(`stderr: ${stderr}`);
+            core.error(`stderr: ${stderr}`);
+            core.setFailed(`Action failed with error`);
             return;
         }
-        console.log(`stdout: ${stdout}`);
+        core.debug(`stdout: ${stdout}`);
     });
 }
 
 // Copy opendaylight maven setting
-exec_command("cp -n ~/.m2/settings.xml{,.orig} ; wget -q -O - https://raw.githubusercontent.com/opendaylight/odlparent/master/settings.xml > ~/.m2/settings.xml");
+exec_command("wget -q -O - https://raw.githubusercontent.com/opendaylight/odlparent/master/settings.xml > ~/.m2/settings.xml");
 
 let options = "";
 
-if (process.env.INPUT_VERBOSE.toLowerCase() == "false") {
+if (core.getInput("verbose").toLowerCase() == "false") {
     options += "--quiet ";
 }
 
-if (process.env.INPUT_SKIPTESTS.toLowerCase() == "true") {
+if (core.getInput("skipTests").toLowerCase() == "true") {
     options += "-DskipTests ";
 }
 
